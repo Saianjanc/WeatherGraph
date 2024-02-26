3847cfc26da551818546c4141fe880416d07e5ca
Saianjan <saianjan1234@gmail.com>
2024-02-26 15:05:59 +0530

    MODIFIED: Today Bar Graph to display temperature!

--- src/Compontents/BarChart.tsx
+++ src/Compontents/BarChart.tsx
@@ -9,16 +9,16 @@ export default function BarChart({ data1 }: { data1: Datum[] }) {
   const accessors = {xAccessor: (d: Datum) => d.dt_txt, yAccessor: (d: Datum) => d.main.temp};
   return (
     <XYChart
-      height={260}
+      height={300}
       width={1200}
       xScale={{ type: 'band', paddingInner: 0.6, paddingOuter: 0.1 }}
       yScale={{ type: 'linear'}}>
       <Grid columns={false} numTicks={5}/>
       <BarSeries dataKey="Line 1" data={data1} {...accessors}/>
-      <Axis orientation="bottom"/>
+      <Axis orientation="bottom" numTicks={data1.length}/>
       <Axis orientation="right"
         hideAxisLine
-        numTicks={5}
+        numTicks={10}
         tickFormat={(value) => value + '°C'}/>
     </XYChart>
   );
